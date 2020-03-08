528d37fa9b3ee3e159bf511b5cc437ea2e9e8c67
jerushachua <jerushachua@gmail.com>
2020-03-07 16:23:13 -0800

    got maps working

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import express from "express";
+import path from "path";
 const app = express();
 const port = 8080; // default port to listen
 
+// Configure Express to use EJS
+app.set( "views", path.join( __dirname, "views" ) );
+app.set( "view engine", "ejs" );
+
+app.use(express.static(__dirname + '../public'));
+
+
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
+    res.render( "index" );
 } );
+
 
 // start the Express server
 app.listen( port, () => {
