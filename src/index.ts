90d4587fbaf6a398694cec15de6bf80ea40d044f
jerushachua <jerushachua@gmail.com>
2020-03-07 19:52:25 -0800

    logo

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import favicon from "serve-favicon";
 const app = express();
 const port = 8080; // default port to listen
 
@@ -8,7 +9,7 @@ app.set( "views", path.join( __dirname, "views" ) );
 app.set( "view engine", "ejs" );
 
 app.use(express.static(__dirname + '../public'));
-
+app.use(favicon(path.join(__dirname, '/public', 'favicon.ico')));
 
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
